9b5217dc223179fbc2c84467fd29f0363c8125c1
Tom Dalenberg <tom@dalenberg.nl>
2025-01-05 14:09:56 +0100

    chore: add ingest and web to docker compose file

--- ingest/src/index.ts
+++ ingest/src/index.ts
@@ -2,7 +2,7 @@ import cluster from 'cluster';
 import os from 'os';
 import { app } from './app';
 
-const port = Number(process.env.INGEST_PORT) || 5000;
+const port = Number(process.env.INGEST_PORT) || 6000;
 const cpuCount = os.cpus().length;
 
 if (cluster.isMaster) {
